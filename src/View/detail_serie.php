822fbecc8ce079b9062046db00f1e17b10af3d67
ibniyamine-chabane <ibni-yamine.chabane@laplateforme.io>
2023-05-28 19:00:25 +0200

    detail for serie done, must adjust on logos companies, it's work

--- src/View/detail_serie.php
+++ src/View/detail_serie.php
@@ -5,12 +5,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../css/style.css">
+    <script defer src="../../js/detail_serie.js"></script>
     <title>detail serie</title>
 </head>
 <body>
     <?php require_once('header.php'); ?>
     <main>
-
+        <section id="detail">
+            <div class="detail-container" id="detail-container">
+                <div class="detail-img-box">
+                    <img id="image-display" src="" alt="">
+                </div>
+                <div class="infos" id="infos">
+                    <h2>titre</h2>
+                    <p>date de sortie</p>
+                    <p>genre <br></p>
+                    <p>pays de production <br></p>
+                    <p>langue <br></p>
+                    <div>
+                        <p>entreprise de production</p>
+                        <div class="companies-container"></div>
+                        <div class="overview"></div>
+                    </div> 
+                </div>
+            </div>
+        </section>
     </main>
 </body>
 </html>
