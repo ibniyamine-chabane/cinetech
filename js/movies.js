8bcdc0b1ff75ccf4a6b22171e6301fa60424c8f2
ibniyamine-chabane <ibni-yamine.chabane@laplateforme.io>
2023-05-31 11:46:53 +0200

    problem in redirection in header fixed + pagination v1 done in movies.js

--- js/movies.js
+++ js/movies.js
@@ -1,64 +1,107 @@
-
 const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MzE1NDdkYWE4MmNhY2ZhODY0ZWUxZjY3ODA0M2QwNyIsInN1YiI6IjY0NjIwMzVmNmUwZDcyMDE1YzBmNzY3NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1toCtEaeEdjRPpSqNHZrOtz7j10GONgCJQ6nPKBuFJg'
-    }
-  };
-  
-// function to formate the date in french way with Date class
-function frenchDate(frenchdateString) {
-    const dateStr = frenchdateString;
-    const date = new Date(dateStr);
-
-    const moisEnFrancais = [
-      'janvier', 'février', 'mars', 'avril', 'mai', 'juin',
-      'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'
-    ];
-
-    const jour = date.getDate().toString().padStart(2, '0');
-    const mois = moisEnFrancais[date.getMonth()];
-    const annee = date.getFullYear().toString();     
-    const dateFormatee = `${jour} ${mois} ${annee}`;
-    return dateFormatee
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MzE1NDdkYWE4MmNhY2ZhODY0ZWUxZjY3ODA0M2QwNyIsInN1YiI6IjY0NjIwMzVmNmUwZDcyMDE1YzBmNzY3NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1toCtEaeEdjRPpSqNHZrOtz7j10GONgCJQ6nPKBuFJg'
   }
+};
 
+// Fonction pour formater la date au format français avec la classe Date
+function frenchDate(frenchdateString) {
+  const dateStr = frenchdateString;
+  const date = new Date(dateStr);
 
-const recupFetch = fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+  const moisEnFrancais = [
+    'janvier', 'février', 'mars', 'avril', 'mai', 'juin',
+    'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'
+  ];
+
+  const jour = date.getDate().toString().padStart(2, '0');
+  const mois = moisEnFrancais[date.getMonth()];
+  const annee = date.getFullYear().toString();
+  const dateFormatee = `${jour} ${mois} ${annee}`;
+  return dateFormatee;
+}
+
+// i initialise first the variable of current page in localstorage for 
+//staying in the same page if i refresh the page.
+let currentPage = localStorage.getItem('currentPage') || 1;
+let totalPages = 0;
+
+function updateCurrentPage() {
+  localStorage.setItem('currentPage', currentPage);
+}
+
+function updatePagination() {
+  const prevButton = document.getElementById('prev');
+  const currentSpan = document.getElementById('current');
+  const nextButton = document.getElementById('next');
+
+  prevButton.textContent = 'Précédent';
+  nextButton.textContent = 'Suivant';
+
+  // if (currentPage === 1) {
+  //   prevButton.classList.add('disabled');
+  // } else {
+  //   prevButton.classList.remove('disabled');
+  // }
+
+  // if (currentPage === totalPages) {
+  //   nextButton.classList.add('disabled');
+  // } else {
+  //   nextButton.classList.remove('disabled');
+  // }
+
+  currentSpan.textContent = `Page ${currentPage} sur ${totalPages}`;
+}
+
+function updateMovies(movies) {
+  const moviesContainer = document.getElementById('movies-container');
+  moviesContainer.innerHTML = '';
+
+  movies.forEach(movie => {
+    const containerCard = document.createElement('div');
+    containerCard.classList.add('movie-card');
+    moviesContainer.appendChild(containerCard);
+
+    const { original_title, poster_path, release_date, id } = movie;
+    containerCard.innerHTML = `<a href="detail/movie/${id}"><img id="movie-image" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="Nom du film"></a>
+                                <h2 id="movie-name">${original_title}</h2>
+                                <p id="movie-date">${frenchDate(release_date)}</p>`;
+  });
+}
+
+function fetchMovies() {
+  const url = `https://api.themoviedb.org/3/movie/popular?language=en-US&page=${currentPage}`;
+  fetch(url, options)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
-
-        data.results.forEach(movies => {
-        
-            const moviesContainer = document.getElementsByClassName('movies-container')[0];
-            const containerCard = document.createElement('div');// a reprendre 
-            containerCard.classList.add('movie-card');
-            moviesContainer.appendChild(containerCard);
-            
-            const {original_title, poster_path, backdrop_path, release_date, id} = movies;
-            containerCard.innerHTML = `<a href="detail/movie/${id}"><img id="movie-image" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="Nom du film"></a>
-                                      <h2 id="movie-name">${original_title}</h2>
-                                      <p id="movie-date">` + frenchDate(release_date) + `</p>`; 
-             
-              // console.log(movies);
-          })
-
-          //pagination part 
-
-          const currentPage = 1;
-          nextPage = currentPage + 1; 
-
-
-          const current = document.getElementById('current');
-          const previous = document.getElementById('prev');
-          const next = document.getElementById('next');
-          current.innerHTML = currentPage;
-          previous.innerHTML = currentPage - 1;
-          next.innerHTML = nextPage;
-          
-          next.addEventListener('click' , () => { const recupFetch = fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=${nextPage}`, options) } )
-    })  
-
+      totalPages = data.total_pages;
+      updateMovies(data.results);
+      updatePagination();
+    })
     .catch(err => console.error(err));
+}
+
+document.addEventListener('DOMContentLoaded', fetchMovies);
+
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+
+
+
+prevButton.addEventListener('click', () => {
+  if (currentPage > 1) {
+    currentPage--;
+    updateCurrentPage()
+    fetchMovies();
+  }
+});
+
+nextButton.addEventListener('click', () => {
+  if (currentPage < totalPages) {
+    currentPage++;
+    updateCurrentPage()
+    fetchMovies();
+  }
+});
--- src/View/header.php
+++ src/View/header.php
@@ -1,8 +1,19 @@
+<?php 
+$host = $_SERVER['REQUEST_URI'];
+// var_dump($host);
+
+// var_dump($id);
+?>
 <header>
     <nav class="navbar">
         <ul>
-            <li><a href="movies">Films</a></li>
-            <li><a href="series">Séries</a></li>
+            <?php if($host == "/cinetech/" || $host == "/cinetech/movies" || $host == "/cinetech/series") :?>
+                <li><a href="movies">Films</a></li>
+                <li><a href="series">Séries</a></li>
+            <?php elseif(isset($id) && $host == "/cinetech/detail/movie/".$id || isset($id) && $host == "/cinetech/detail/serie/".$id) :?>
+                <li><a href="../../movies">Films</a></li>
+                <li><a href="../../series">Séries</a></li>
+            <?php endif; ?>
         </ul>
     </nav>
 </header>
